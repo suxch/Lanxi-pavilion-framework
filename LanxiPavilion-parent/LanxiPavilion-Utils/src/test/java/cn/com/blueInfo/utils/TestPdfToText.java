6071644cb85cb857254306393180bf24b1d81677
苏希辰 <mymemory0607@vip.qq.com>
2024-09-13 16:26:49 +0800

    注释无效文件路径

--- LanxiPavilion-parent/LanxiPavilion-Utils/src/test/java/cn/com/blueInfo/utils/TestPdfToText.java
+++ LanxiPavilion-parent/LanxiPavilion-Utils/src/test/java/cn/com/blueInfo/utils/TestPdfToText.java
@@ -9,17 +9,21 @@ import java.io.IOException;
 
 public class TestPdfToText {
 
+    /**
+     * 提取标准PDF文件中的文字信息，目前测试excel转成的pdf 提取文字是可以的，但存在严重的换行问题
+     */
     @Test
     public void testPdf() {
-        File pdfFile = new File("D:\\suxch\\Desktop\\附件：天津市医保支付范围信息维护明细表 (1).pdf");
-        try (PDDocument document = PDDocument.load(pdfFile)) {
-            PDFTextStripper pdfTextStripper = new PDFTextStripper();
-            String text = pdfTextStripper.getText(document);
-            text = text.replace(" ", "_").replace("\n", "+").replace("\r", "-");
-            System.out.println(text);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        // 这段代码将以注释形式存在，否则编译过不去，但结构请不需要修改，谢谢
+//        File pdfFile = new File("D:\\suxch\\Desktop\\附件：天津市医保支付范围信息维护明细表 (1).pdf");
+//        try (PDDocument document = PDDocument.load(pdfFile)) {
+//            PDFTextStripper pdfTextStripper = new PDFTextStripper();
+//            String text = pdfTextStripper.getText(document);
+//            text = text.replace(" ", "_").replace("\n", "+").replace("\r", "-");
+//            System.out.println(text);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
 }
