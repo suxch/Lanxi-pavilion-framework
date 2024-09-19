3e08b5489e2a4b03db6bcb767d775bd77979823b
苏希辰 <mymemory0607@vip.qq.com>
2024-09-19 14:57:14 +0800

    创建启动类及相关启动依赖

---
+++ LanxiPavilion-parent/LanxiPavilion-Starter/src/main/java/cn/com/blueInfo/starter/ServletInitializer.java
@@ -0,0 +1,12 @@
+package cn.com.blueInfo.starter;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LanxiPavilionApplication.class);
+    }
+}
